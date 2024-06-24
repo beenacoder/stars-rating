d022b13e543b51143534cd301a31ffd6a07622e7
Ariel Cabeza <cervanteslibch@gmail.com>
2024-06-24 20:37:52 -0300

    done

--- src/component/Rating.jsx
+++ src/component/Rating.jsx
@@ -1,8 +1,44 @@
+import { useState } from "react"
+import { FaStar } from "react-icons/fa"
 
 
-function Rating() {
+function Rating({stars = 10}) {
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+
+
+    const handleClick = (getIndex) => {
+        setRating(getIndex)
+    }
+
+
+    const handleMouseOn = (getIndex) => {
+        setHover(getIndex);
+    }
+
+
+    const handleMouseOff = (getIndex) => {
+        setHover(rating)
+    }
+
+
+
   return (
-    <div>Rating</div>
+    <div className="star-rating">
+{
+    [...Array(stars)].map((_, index) => {
+        index += 1;
+        return <FaStar 
+            key={index}
+            className={index <= (hover || rating) ? 'on' : 'off' }
+            onClick={() => {handleClick(index)}}
+            onMouseMove={() => {handleMouseOn(index)}}
+            onMouseLeave={() => {handleMouseOff(index)}}
+            size={40}
+        />
+    })
+}
+    </div>
   )
 }
 
